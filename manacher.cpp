182e22282439cf2caf7d67cd4cfa540159d623dd
Stjepan Glavina <stjepang@gmail.com>
2016-09-26 00:19:07 +0200

    Documentation for Manacher's algorithm

--- manacher.cpp
+++ manacher.cpp
@@ -1,18 +1,21 @@
 // Finds all palindromes in a string
 //
-// Configure: konstanta MAX
-// Note: The elements are of char type
-// Note: Special elements are -1
-// Note: rad must be of length 2*MAX
+// Given a string s of length N, finds all palindromes as its substrings.
 //
-// manacher(s, N, rad): Computes a sequence of radii
-// rad[2*i]:   Radius of the palindrome centered in s[i]
-// rad[2*i+1]: Radius of hte palindrome centered in s[i..i+1]
+// After calling manacher(s, N, rad), rad[x] will be the radius of the largest
+// palindrome centered at index x / 2.
+// Example:
+//   s   = b a n a n a a
+//   rad = 0000102010010
 //
-// Example: s   = b a n a n a a
-//          rad = 0000102010010
-
-int rad[2*MAX];
+// Note: Array rad must be of length at least twice the length of the string.
+// Also, "invalid" characters are denoted by -1, therefore the string must not
+// contain such characters.
+//
+// Time complexity: O(N)
+//
+// Constants to configure:
+// - MAX is the maximum length of the string
 
 void manacher(char *s, int N, int *rad) {
   static char t[2*MAX];
